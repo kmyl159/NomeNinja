92c040acecdd21535ec10c384591af11e26ce06d
Camilo Lopes <Camilo_m_lopes@hotmail.com>
2022-05-18 06:13:47 -0300

    teste

--- Components/image.js
+++ Components/image.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import mypic from '../public/img/background.jpg'
+import mypic from '../public/img/Background.jpg'
 const MyImage = (props) => {
   return (
     <Image
